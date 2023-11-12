835d667c63940cd103556feecfbfa21929a7b9f1
pogorilla <rocky0x007@gmail.com>
2023-11-12 02:29:25 +0200

    landing page

---
+++ landing_page/script.js
@@ -0,0 +1,12 @@
+title = "InsightForge - Where vision meets precision ";
+position = 0;
+function scrolltitle() {
+    document.title = title.substring(position, title.length) + title.substring(0, position); 
+    position++;
+    if (position > title.length) position = 0;
+    titleScroll = window.setTimeout(scrolltitle,300);
+}
+scrolltitle();
+
+
+
